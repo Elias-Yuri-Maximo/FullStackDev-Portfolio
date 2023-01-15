cdb3dc1fad677f21da43a86520f4405778db5e9e
Elias Maximo <eliasmaximus@gmail.com>
2023-01-15 16:36:57 -0300

    Second Commit

---
+++ week2/cms/src/app/contacts/contacts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-contacts',
+  templateUrl: './contacts.component.html',
+  styleUrls: ['./contacts.component.css']
+})
+export class ContactsComponent {
+
+}
